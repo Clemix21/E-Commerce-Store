c5ba939ebacf10ee521afdb4746843f8dc000863
Clemens <92445409+Clemix21@users.noreply.github.com>
2025-07-22 16:35:45 +0200

    added coupons

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,8 @@ import cookieParser from 'cookie-parser';
 // Importing Routes
 import authRoutes from './routes/auth.route.js';
 import productRoutes from './routes/product.route.js';
-import cartRoutes from './routes/cart.route.js';
+import cartRoutes from './routes/cart.routes.js';
+import couponRoutes from './routes/coupon.route.js';
 
 // Importing Database Connection
 import { connectDB } from './lib/db.js';
@@ -19,8 +20,9 @@ app.use(express.json()); // Middleware to parse body of requests
 app.use(cookieParser()); // Middleware to parse cookies
 
 app.use("/api/auth", authRoutes)
-app.use("/api/products", productRoutes);
-app.use("/api/cart", cartRoutes);
+app.use("/api/products", productRoutes)
+app.use("/api/cart", cartRoutes)
+app.get("/", couponRoutes)
 
 app.listen(PORT, () => {
   console.log('Server is running on port http://localhost:' + PORT);
